a96b34781ed2fb481569f7de7a7aab89f8371917
Leandro <ocel99@gmail.com>
2024-06-09 22:17:12 -0300

    Aula3_3

---
+++ NetBeansProjects/Aula3_3/src/ShoppingCart01.java
@@ -0,0 +1,12 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+     String custName = "Alex";
+     String itemDesc = "Camisa";
+     String mensagem = custName + " quer comprar uma " + itemDesc;
+        
+        
+        System.out.println(mensagem);
+        
+    }
+}
